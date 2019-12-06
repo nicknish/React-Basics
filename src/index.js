a5290f599c8e0f5ddedc7324b09ba079e753fae2
Nick Nish <nickjnish@gmail.com>
2019-12-05 23:30:33 -0800

    Step 2: Create a wrapper element
    Demonstrate how to wrap multiple elements created with React.createElement

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-var headerElement = React.createElement("h1", { id: "title" }, "Nick Nish");
+var name = "Nick Nish";
+var handle = "@nickjnish";
 
-ReactDOM.render(headerElement, document.getElementById("app"));
+var headerElement = React.createElement("h1", { id: "title" }, name);
+var handleElement = React.createElement("h3", {}, handle);
+
+const wrapperElement = React.createElement(
+  "div",
+  { id: "container" },
+  headerElement,
+  handleElement
+);
+
+ReactDOM.render(wrapperElement, document.getElementById("app"));
