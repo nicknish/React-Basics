93820cde01c630a700b4071ce3dd67b66c4072f9
Nick Nish <nickjnish@gmail.com>
2019-12-06 00:21:16 -0800

    Exercise 1: Update props of the Profile component with your own info!

---
+++ src/Exercises/exercise-1-props.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function NameComponent(props) {
+  return <h1>Name: {props.name}</h1>;
+}
+
+function HandleComponent(props) {
+  return <h3>Handle: {props.handle}</h3>;
+}
+
+function AvatarComponent(props) {
+  return <img src={props.src} />;
+}
+
+// TODO: Fill the props with your own info!
+function App() {
+  return (
+    <div id="container">
+      <NameComponent name="" />
+      <HandleComponent handle="" />
+      <AvatarComponent src="" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import "./styles.css";
 // require("./Steps/step-2");
 // require("./Steps/step-3");
 // require("./Steps/step-4");
-require("./Steps/step-5");
+// require("./Steps/step-5");
+require("./Exercises/exercise-1-props");
