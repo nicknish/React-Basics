fbb7583e6b4433949e180ed5b280b5839de81b11
Nick Nish <nickjnish@gmail.com>
2019-12-08 01:14:40 -0800

    Step 8: Demo combining class and functional components

--- src/index.js
+++ src/index.js
@@ -10,4 +10,5 @@ import "./styles.css";
 // require("./Solutions/solution-1-props");
 
 // require("./Steps/step-6");
-require("./Steps/step-7");
+// require("./Steps/step-7");
+require("./Steps/step-8");
