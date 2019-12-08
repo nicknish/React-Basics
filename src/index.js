05e646381f3f892410ba80463c9bc834af7d56d8
Nick Nish <nickjnish@gmail.com>
2019-12-08 01:47:49 -0800

    Reorganize to more easily sequence steps

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import "./styles.css";
 
-// require("./Steps/step-1");
-// require("./Steps/step-2");
-// require("./Steps/step-3");
-// require("./Steps/step-4");
-// require("./Steps/step-5");
+// require("./Steps/Steps-1-9/step-1");
+// require("./Steps/Steps-1-9/step-2");
+// require("./Steps/Steps-1-9/step-3");
+// require("./Steps/Steps-1-9/step-4");
+// require("./Steps/Steps-1-9/step-5");
 
 // require("./Exercises/exercise-1-props");
 // require("./Solutions/solution-1-props");
 
-// require("./Steps/step-6");
-// require("./Steps/step-7");
-// require("./Steps/step-8");
-// require("./Steps/step-9");
-// require("./Steps/step-10");
-require("./Steps/step-11");
+// require("./Steps/Steps-1-9/step-6");
+// require("./Steps/Steps-1-9/step-7");
+// require("./Steps/Steps-1-9/step-8");
+// require("./Steps/Steps-1-9/step-9");
+// require("./Steps/Steps-11-19/step-10");
+require("./Steps/Steps-11-19/step-11");
